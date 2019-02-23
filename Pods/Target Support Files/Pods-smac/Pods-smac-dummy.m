d49da8df6103c2259b8a8e97f6b8c8372bbf9c25
Mubarak Almahmeed <mubarakalmahmeed@192-168-1-52.rev.bb.zain.com>
2019-02-23 19:21:37 +0300

    Lesson 1

---
+++ Pods/Target Support Files/Pods-smac/Pods-smac-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_smac : NSObject
+@end
+@implementation PodsDummy_Pods_smac
+@end
---
+++ smac/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  smac
+//
+//  Created by Mubarak Almahmeed on 2/21/19.
+//  Copyright © 2019 Mubarak Almahmeed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
