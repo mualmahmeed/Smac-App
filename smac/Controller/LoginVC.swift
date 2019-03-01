6f5c9c2a0351dac802f93d3c2d54b331f31d7043
Mubarak <almahmeedmu@gmail.com>
2019-03-01 08:31:11 +0300

    Lesson 5

---
+++ smac/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  smac
+//
+//  Created by Mubarak Almahmeed on 3/1/19.
+//  Copyright © 2019 Mubarak Almahmeed. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+}
---
+++ smac/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  smac
+//
+//  Created by Mubarak Almahmeed on 3/1/19.
+//  Copyright © 2019 Mubarak Almahmeed. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN = "toLogin"
+
